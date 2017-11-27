44c7d247f1a36d2f223961d1888db24c5b5a38da
Winn Y Cui <pyrinox@airbears2-10-142-66-9.airbears2.1918.berkeley.edu>
2017-11-27 01:47:58 -0800

    edit server.js to not hardcode port to fix heroku app error

--- server.js
+++ server.js
@@ -187,4 +187,5 @@ function getUsers(){
 
 
 //webpage has a function "listen" that listens to localhost:3000
-webpage.listen(3000);
+// webpage.listen(3000);
+webpage.listen(process.env.PORT || 5000);
